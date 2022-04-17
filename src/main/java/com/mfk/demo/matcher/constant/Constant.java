688e93ff128d763ed37f6ccc7d810b7675b0f9ea
Md Farhan Khan <mdfarhan.khan@infosys.com>
2022-04-17 20:05:04 +0530

    adder csv files

--- src/main/java/com/mfk/demo/matcher/constant/Constant.java
+++ src/main/java/com/mfk/demo/matcher/constant/Constant.java
@@ -3,8 +3,7 @@ package com.mfk.demo.matcher.constant;
 public abstract class Constant {
 
     public static final String NUMBER_SCORER = "NUMBER_SCORER";
-    public static final String DATE_SCORER = "NUMBER_SCORER";
-    public static final String STRING_SCORER = "NUMBER_SCORER";
-
+    public static final String DATE_SCORER = "DATE_SCORER";
+    public static final String STRING_SCORER = "STRING_SCORER";
     public static final String TRANSACTION_SCORER = "TRANSACTION_SCORER";
 }
