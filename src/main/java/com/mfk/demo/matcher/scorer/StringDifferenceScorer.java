d746474f340907b10656406228a53acf8ae52abc
Md Farhan Khan <mdfarhan.khan@infosys.com>
2022-04-17 15:40:09 +0530

    updated the stringdifferencescorer with edit distance algo

--- src/main/java/com/mfk/demo/matcher/scorer/StringDifferenceScorer.java
+++ src/main/java/com/mfk/demo/matcher/scorer/StringDifferenceScorer.java
@@ -11,8 +11,10 @@ public class StringDifferenceScorer implements IDifferenceScorer<String> {
 
     /**
      * Return the difference score of two String
-     * The difference score is the number of insertions required to reach one string from another
-     * If one string in not the sequence of another, it returns -1 meaning they do not match.
+     * The difference score is the number of insertions required to reach one string
+     * from another
+     * If one string in not the sequence of another, it returns -1 meaning they do
+     * not match.
      *
      * @param source
      * @param destination
@@ -20,8 +22,55 @@ public class StringDifferenceScorer implements IDifferenceScorer<String> {
      */
     @Override
     public Double getScore(Threshold threshold, String source, String destination) {
-       //TODO: Edit distance
-        return -1.0;
+        int diff = editDistance(source, destination);
+        return diff>threshold.getStringThreshold()? -1.0 : diff;
+    }
+
+    private int editDistance(String source, String destination) {
+        // Create a table to store results of subproblems
+        int sourceLength = source.length();
+        int destinationLength = destination.length();
+        int dp[][] = new int[sourceLength + 1][destinationLength + 1];
+
+        // Fill d[][] in bottom up manner
+        for (int i = 0; i <= sourceLength; i++) {
+            for (int j = 0; j <= destinationLength; j++) {
+                // If first string is empty, only option is
+                // to insert all characters of second string
+                if (i == 0){
+                    dp[i][j] = j; // Min. operations = j
+                }
+                // If second string is empty, only option is
+                // to remove all characters of second string
+                else if (j == 0){
+                    dp[i][j] = i; // Min. operations = i
+                }
+                // If last characters are same, ignore last
+                // char and recur for remaining string
+                else if (source.charAt(i - 1) == destination.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                // If the last character is different,
+                // consider all possibilities and find the
+                // minimum
+                else
+                    dp[i][j] = 1
+                            + min(dp[i][j - 1], // Insert
+                                    dp[i - 1][j], // Remove
+                                    dp[i - 1][j - 1]); // Replace
+            }
+        }
+        return dp[sourceLength][destinationLength];
+    }
+
+    private int min(int x, int y, int z) {
+        if (x <= y && x <= z){
+            return x;
+        }
+        if (y <= x && y <= z){
+            return y;
+        }
+            return z;
     }
 
 }
--- src/test/java/com/mfk/demo/matcher/CsvMatcherApplicationTests.java
+++ src/test/java/com/mfk/demo/matcher/CsvMatcherApplicationTests.java
@@ -1,8 +1,9 @@
 package com.mfk.demo.matcher;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class CsvMatcherApplicationTests {
 
 	@Test
